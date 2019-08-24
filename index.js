3aece400b6066d5091ce99d734309cc228641001
Stephanie Weber <sweber@thoughtworks.com>
2019-08-24 12:26:43 -0700

    Add download button and render certificate as canvas

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
-function showMarkerJs(img) {
-    let mark = new markerjs.MarkerArea(img);
-    mark.show((dataUrl) => {
-        img.src = dataUrl;
-    })
+function createDownloadButton(url) {
+    var a = document.createElement('a');
+    a.download = url;
+    a.className = 'btn btn-primary';
+    a.href = url;
+    a.textContent = 'Download certificate';
+    document.getElementById('nav').appendChild(a);
 }
+
+function renderNewCertificate() {
+    let canvas=document.getElementById("canvas");
+    canvas.width = 4266;
+    canvas.height = 3200;
+    let context = canvas.getContext('2d');
+    let image=new Image();
+    image.src = "certificate.png";
+    image.onload = function() {
+        context.drawImage(image, 0, 0);
+    };
+    createDownloadButton("");
+}
+
+document.addEventListener('DOMContentLoaded', renderNewCertificate, false);
+
+
