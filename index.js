3dce305add05a0ea1ff8600ffe67cdc44e573be3
Stephanie Weber <sweber@thoughtworks.com>
2019-08-25 08:39:38 -0700

    Add name and course to certificate; ignore .DS_Store

--- index.js
+++ index.js
@@ -1,25 +1,52 @@
+let courses = {
+    0: "",
+    1: "Feedback",
+    2: "Poetry",
+    3: "Design Thinking",
+    4: "This title is really long and I don't think it has to be"
+};
+
+function addText(context, text, x, y) {
+
+}
+
 function createDownloadButton(url) {
-    var a = document.createElement('a');
-    a.download = url;
+    let a = document.createElement('a');
     a.className = 'btn btn-primary';
-    a.href = url;
+    a.download = 'VirgilCon2019.png';
+    a.href = url.toDataURL();
     a.textContent = 'Download certificate';
     document.getElementById('nav').appendChild(a);
 }
 
-function renderNewCertificate() {
-    let canvas=document.getElementById("canvas");
-    canvas.width = 4266;
-    canvas.height = 3200;
-    let context = canvas.getContext('2d');
-    let image=new Image();
-    image.src = "certificate.png";
-    image.onload = function() {
-        context.drawImage(image, 0, 0);
-    };
-    createDownloadButton("");
+function setAttributes(canvas, image, context) {
+    canvas.width = image.width;
+    canvas.height = image.height;
+    context.font = 'italic 100pt Calibri';
+    context.fillStyle = "black";
 }
 
-document.addEventListener('DOMContentLoaded', renderNewCertificate, false);
+function renderBlankCertificate() {
+    renderNewCertificate("", 0);
+}
+
+function renderNewCertificate(name, course) {
+    let canvas = document.getElementById("canvas");
+    let context = canvas.getContext('2d');
+    let image = new Image();
+    image.src = "certificate.png";
+    image.onload = function() {
+        setAttributes(canvas, image, context);
+        context.drawImage(image, 0, 0);
+        context.fillText(name, 1800, 1500);
+        context.fillText(courses[course], 1900, 2000);
+        // addText(context, "Some name", 922, 1530);
+        // x = imageWidth-textWidth/2
+        createDownloadButton(canvas);
+    };
+}
+
+document.addEventListener('DOMContentLoaded', renderBlankCertificate, false);
+
 
 
